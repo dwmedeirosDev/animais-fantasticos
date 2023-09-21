be461afe3bb1b8f80e07734e081c0752ba272e7a
David Medeiros <davidmedeiros94@hotmail.com>
2023-09-21 20:22:38 -0300

    JS

---
+++ script.js
@@ -0,0 +1,24 @@
+// Retorne no console todas as imagens do site
+// R: const img = document.querySelectorAll("img");
+// console.log(img);
+// ###
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+// R: const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]');
+// console.log(imagensAnimais);
+// ###
+
+// Selecione todos os links internos
+// R: const linkInterno = document.querySelectorAll("[href^='#']");
+// console.log(linkInterno);
+// ###
+
+// Selecione o primeiro H2 dentro de .animais-descricao
+// R: const primeiro = document.querySelector(".animais-descricao h2");
+// console.log(primeiro);
+// ###
+
+// Selecione o último P do site
+// R: const paragrafo = document.querySelectorAll("p");
+// console.log(paragrafo[--paragrafo.length]);
+// ###
