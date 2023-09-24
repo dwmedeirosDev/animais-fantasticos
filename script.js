5d196da0e7932f329d0cf85626f09f8a23676081
David Medeiros <davidmedeiros94@hotmail.com>
2023-09-24 14:55:30 -0300

    Classes e Atributos

--- script.js
+++ script.js
@@ -51,3 +51,37 @@
 //   console.log(i++);
 // });
 // ###
+
+// Exercício Classes e Atributos
+
+// Adicione a classe ativo a todos os itens do menu
+// R: const itensMenu = document.querySelectorAll(".menu a");
+// itensMenu.forEach((item) => {
+//   item.classList.add("ativo");
+// });
+// ###
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+// R: const itensMenu = document.querySelectorAll(".menu a");
+// itensMenu.forEach((item) => {
+//   item.classList.add("ativo");
+// });
+// itensMenu.forEach((item) => {
+//   item.classList.remove("ativo");
+// });
+// itensMenu[0].classList.add("ativo");
+// ###
+
+// Verifique se as imagens possuem o atributo alt
+// R: const imgs = document.querySelectorAll("img");
+// imgs.forEach((img) => {
+//   const possuiAtt = img.hasAttribute("alt");
+//   console.log(possuiAtt);
+// });
+// ###
+
+// Modifique o href do link externo no menu
+// R: const link = document.querySelector("a[href^='http']");
+// link.setAttribute("href", "google.com");
+// console.log(link);
+// ###
